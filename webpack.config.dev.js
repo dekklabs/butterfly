ddf0844b167355929bae30cb328eb3b842b2f6ad
Jonathan Jacobo Romero <dekkservidores@gmail.com>
2020-07-04 21:32:29 -0500

    Fix file-loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,19 +1,14 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const URL = "https://noticias.dev"
-
 module.exports = {
-  entry: [
-    "webpack-dev-server/client?" + URL + ':5001',
-    "./src/index.js"
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: URL + ":5001/"
+  entry: {
+    app: ["@babel/polyfill", "./src/index.js"]
   },
-  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.bundle.js"
+  },
   module: {
     rules: [
       {
@@ -21,49 +16,73 @@ module.exports = {
         loader: "babel-loader"
       },
       {
-        test: /\.(scss|sass)$/,
-        exclude: /module\.s?css$/,
+        test: /\.(s*)css$/,
         use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader',
+          MiniCssExtractPlugin.loader,
+          "css-loader", 
+          "sass-loader"
         ]
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
+            {
+                loader: "file-loader",
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: './images/',
+                    esModule: false
+                }
+            },
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                  quality: 65
+                },
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: [0.65, 0.90],
+                  speed: 4
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                webp: {
+                  quality: 75
+                },
+              }
+            },
+        ],
+      },
+      {
+        test: /\.(woff(2)?||ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
             options: {
-              bypassOnDebug: true,
-              disable: true,
-              output: 'images/public'
+                name: '[name].[ext]',
+                outputPath: 'fonts/',
+                esModule: false
             }
-          }
-        ]
+        }]
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "css/app.bundle.css"
+      filename: "app.bundle.css",
+      chunkFilename: '[id].css',
+      ignoreOrder: false
     })
   ],
-  devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
-    publicPath: URL + ":5001/",
-    port: 3001,
-    hot: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,OPTIONS,HEAD,PUT,POST,DELETE,PATCH',
-      'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization, X-Request-With',
-      'Access-Control-Allow-Credentials': 'true',
-    },
-  },
+  performance: {
+    maxEntrypointSize: 400000,
+    maxAssetSize: 100000,
+    assetFilter: (asset) => {
+      return asset.match('entrypoint1.js');
+    }
+  }
 };
