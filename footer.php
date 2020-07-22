262cb15a4f940464813c7c5e94b8d291846695a4
Jonathan Jacobo Romero <dekkservidores@gmail.com>
2020-07-22 12:16:14 -0500

    Update name app.bundle.js

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		<footer>
 			testing
 		</footer>
-		<!--script src="<?php echo get_site_url() ?>/dist/bundle.js"></script -->
+		<!--script src="<?php echo esc_url( get_template_directory_uri() ); ?>/dist/app.bundle.js"></script-->
 		<script src="<?php echo get_site_url() ?>:5001/bundle.js"></script>
 		<?php wp_footer(); ?>
 		</main>
